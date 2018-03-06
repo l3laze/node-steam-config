a3b380ec2173444e3673a5e4f4ce0c11694f2514
Tom Shaver <l3l_aze@yahoo.com>
2018-03-06 02:06:51 -0600

    OMG
    =(

--- perms.sh
+++ perms.sh
@@ -4,9 +4,9 @@ stage=$1
 
 # Partially based on https://stackoverflow.com/a/394247/7665043
 
-if [[ "$stage" == 'script' ]]; then
+if [ "$stage" == 'script' ]; then
   unamestr=`uname`
-  if [[ "$unamestr" == 'Linux' ]]; then
+  if [ "$unamestr" == 'Linux' ]; then
     ls -la /home/travis/build/l3laze/node-steam-config/node_modules/steam-dummy/data/Linux
     chmod -R u+rwx /home/travis/build/l3laze/node-steam-config/
   else
