cc16a2f99bab160ecd86037c91d8a59bce69fa28
Tom Shaver <l3l_aze@yahoo.com>
2018-04-03 18:02:50 -0500

    Bugfix
    Testing code was running on my real Steam installation locally...lol. Just had to rearrange some tests.

--- test/index.test.js
+++ test/index.test.js
@@ -34,6 +34,8 @@ if (typeof process.env.CI !== 'undefined' || process.env.SCTRP === true) { // Te
   pathTo = path.join(__dirname, 'Dummy')
 }
 
+console.info(`Testing with path ${pathTo}`)
+
 describe('Module SteamConfig @notreq', function moduleDescriptor () {
   this.slow(0)
 
@@ -47,110 +49,6 @@ describe('Module SteamConfig @notreq', function moduleDescriptor () {
       steam.paths.root = pathTo
       steam.paths.id64 = id64
       steam.paths.accountId = accountID
-    })
-  })
-
-  describe('#load', function loadDescriptor () {
-    this.timeout(4000)
-
-    it('should load single entries as requested', async function loadProperly () {
-      await steam.load(steam.paths.appinfo)
-      expect(steam.appinfo).to.be.a('array')
-
-      await steam.load(steam.paths.config)
-      expect(steam.config).to.be.a('object').and.have.property('InstallConfigStore')
-      /*  .and.have.property('Software')
-          .and.have.property('Valve')
-          .and.have.property('Steam')
-          .and.have.property('Accounts')
-        expect(steam.config.InstallConfigStore).to.not.have.property('DownloadRates')
-        expect(steam.config.InstallConfigStore.Software.Valve.Steam).to.not.have.property('')
-      */
-
-      await steam.load(steam.paths.libraryfolders)
-      expect(steam.libraryfolders).to.be.a('object').and.have.property('LibraryFolders')
-
-      await steam.load(steam.paths.localconfig)
-      expect(steam.localconfig).to.be.a('object').and.have.property('UserLocalConfigStore')
-
-      await steam.load(steam.paths.loginusers)
-      expect(steam.loginusers).to.be.a('object').and.have.property('users')
-
-      await steam.load(steam.paths.registry)
-      expect(steam.registry).to.be.a('object').and.have.property('Registry')
-
-      await steam.load(steam.paths.sharedconfig)
-      expect(steam.sharedconfig).to.be.a('object').and.have.property('UserRoamingConfigStore')
-
-      await steam.load(steam.paths.shortcuts)
-      expect(steam.shortcuts).to.be.a('object').and.have.property('shortcuts')
-
-      await steam.load(steam.paths.skins)
-      expect(steam.skins).to.be.a('array')
-
-      await steam.load(steam.paths.steamapps())
-      expect(steam.apps).to.be.a('array').and.have.length(2)
-    })
-
-    it('should load steamapps as requested', async function loadMoreApps () {
-      let externalLib
-
-      for (let k of Object.keys(steam.libraryfolders.LibraryFolders)) {
-        if (k !== 'ContentStatsID' && k !== 'TimeNextStatsReport') {
-          steam.libraryfolders.LibraryFolders[ k ] = path.join(pathTo, 'External Steam Library Folder')
-          externalLib = steam.libraryfolders.LibraryFolders[ k ]
-        }
-      }
-
-      await steam.load(steam.paths.steamapps(externalLib))
-      expect(steam.apps).to.be.a('array').and.have.property('length').and.equal(4)
-
-      await steam.load(steam.paths.steamapps())
-      expect(steam.apps).to.be.a('array').and.have.property('length').and.equal(4)
-
-      steam.append = false
-      await steam.load(steam.paths.steamapps())
-      expect(steam.apps).to.be.a('array').and.have.property('length').and.equal(2)
-    })
-
-    it('should load multiple/all entries as requested', async function loadProperly () {
-      try {
-        const everything = steam.paths.all
-        steam.apps.length = 0
-
-        await steam.load(everything)
-        await steam.load(steam.paths.steamapps())
-        expect(steam.appinfo).to.be.a('array')
-        expect(steam.config).to.be.a('object').and.have.property('InstallConfigStore')
-        expect(steam.libraryfolders).to.be.a('object').and.have.property('LibraryFolders')
-        expect(steam.localconfig).to.be.a('object').and.have.property('UserLocalConfigStore')
-        expect(steam.loginusers).to.be.a('object').and.have.property('users')
-        expect(steam.registry).to.be.a('object').and.have.property('Registry')
-        expect(steam.sharedconfig).to.be.a('object').and.have.property('UserRoamingConfigStore')
-        expect(steam.shortcuts).to.be.a('object').and.have.property('shortcuts')
-        expect(steam.skins).to.be.a('array')
-        expect(steam.apps).to.be.a('array').and.have.length(2)
-      } catch (err) {
-        throw err
-      }
-    })
-  })
-
-  describe('#save', function saveDescriptor () {
-    it('should save entries as requested', async function saveProperly () {
-      expect(async function saveWorks () {
-        await steam.save(steam.paths.config)
-        await steam.save(steam.paths.libraryfolders)
-        await steam.save(steam.paths.localconfig)
-        await steam.save(steam.paths.registry)
-        await steam.save(steam.paths.sharedconfig)
-      }).to.not.throw()
-    })
-
-    it('should save multiple entries as requested', async function saveProperly () {
-      expect(async function saveWorks () {
-        await steam.save(steam.paths.all)
-      }).to.not.throw()
     })
   })
 
@@ -280,6 +178,103 @@ describe('Module SteamConfig @notreq', function moduleDescriptor () {
       await steam.setUser('l3l_aze')
       expect(steam.paths.id64).to.equal('76561198067577712')
       expect(steam.paths.accountId).to.equal('107311984')
+    })
+  })
+
+  describe('#load', function loadDescriptor () {
+    this.timeout(4000)
+
+    it('should load single entries as requested', async function loadProperly () {
+      await steam.load(steam.paths.appinfo)
+      expect(steam.appinfo).to.be.a('array')
+
+      await steam.load(steam.paths.config)
+      expect(steam.config).to.be.a('object').and.have.property('InstallConfigStore')
+
+      await steam.load(steam.paths.libraryfolders)
+      expect(steam.libraryfolders).to.be.a('object').and.have.property('LibraryFolders')
+
+      await steam.load(steam.paths.localconfig)
+      expect(steam.localconfig).to.be.a('object').and.have.property('UserLocalConfigStore')
+
+      await steam.load(steam.paths.loginusers)
+      expect(steam.loginusers).to.be.a('object').and.have.property('users')
+
+      await steam.load(steam.paths.registry)
+      expect(steam.registry).to.be.a('object').and.have.property('Registry')
+
+      await steam.load(steam.paths.sharedconfig)
+      expect(steam.sharedconfig).to.be.a('object').and.have.property('UserRoamingConfigStore')
+
+      await steam.load(steam.paths.shortcuts)
+      expect(steam.shortcuts).to.be.a('object').and.have.property('shortcuts')
+
+      await steam.load(steam.paths.skins)
+      expect(steam.skins).to.be.a('array')
+
+      await steam.load(steam.paths.steamapps())
+      expect(steam.apps).to.be.a('array').and.have.length(2)
+    })
+
+    it('should load steamapps as requested', async function loadMoreApps () {
+      let externalLib
+
+      for (let k of Object.keys(steam.libraryfolders.LibraryFolders)) {
+        if (k !== 'ContentStatsID' && k !== 'TimeNextStatsReport') {
+          steam.libraryfolders.LibraryFolders[ k ] = path.join(pathTo, 'External Steam Library Folder')
+          externalLib = steam.libraryfolders.LibraryFolders[ k ]
+        }
+      }
+
+      await steam.load(steam.paths.steamapps(externalLib))
+      expect(steam.apps).to.be.a('array').and.have.property('length').and.equal(4)
+
+      await steam.load(steam.paths.steamapps())
+      expect(steam.apps).to.be.a('array').and.have.property('length').and.equal(4)
+
+      steam.append = false
+      await steam.load(steam.paths.steamapps())
+      expect(steam.apps).to.be.a('array').and.have.property('length').and.equal(2)
+    })
+
+    it('should load multiple/all entries as requested', async function loadProperly () {
+      try {
+        const everything = steam.paths.all
+        steam.apps.length = 0
+
+        await steam.load(everything)
+        await steam.load(steam.paths.steamapps())
+        expect(steam.appinfo).to.be.a('array')
+        expect(steam.config).to.be.a('object').and.have.property('InstallConfigStore')
+        expect(steam.libraryfolders).to.be.a('object').and.have.property('LibraryFolders')
+        expect(steam.localconfig).to.be.a('object').and.have.property('UserLocalConfigStore')
+        expect(steam.loginusers).to.be.a('object').and.have.property('users')
+        expect(steam.registry).to.be.a('object').and.have.property('Registry')
+        expect(steam.sharedconfig).to.be.a('object').and.have.property('UserRoamingConfigStore')
+        expect(steam.shortcuts).to.be.a('object').and.have.property('shortcuts')
+        expect(steam.skins).to.be.a('array')
+        expect(steam.apps).to.be.a('array').and.have.length(2)
+      } catch (err) {
+        throw err
+      }
+    })
+  })
+
+  describe('#save', function saveDescriptor () {
+    it('should save entries as requested', async function saveProperly () {
+      expect(async function saveWorks () {
+        await steam.save(steam.paths.config)
+        await steam.save(steam.paths.libraryfolders)
+        await steam.save(steam.paths.localconfig)
+        await steam.save(steam.paths.registry)
+        await steam.save(steam.paths.sharedconfig)
+      }).to.not.throw()
+    })
+
+    it('should save multiple entries as requested', async function saveProperly () {
+      expect(async function saveWorks () {
+        await steam.save(steam.paths.all)
+      }).to.not.throw()
     })
   })
 })
